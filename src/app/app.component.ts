3cc0e2404aa75bdb1c4e0fa640aa69b9815f0984
Eugene <chepurko0@gmail.com>
2018-10-17 15:09:19 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div>
+    
+      <pm-products></pm-products>
+    </div>
+    `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pageTitle = 'Acme Product Management';
+}
---
+++ src/app/myArrStr.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+Component({
+    selector: 'str-arr',
+    templateUrl: './myArrStrPage.html',
+    styleUrls: ['./app.component.css']
+  })
+  
+var colors :string [] = ["reg","green","blue"];
